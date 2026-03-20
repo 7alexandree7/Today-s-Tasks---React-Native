deaa0111726eaac003900b7052dfbbd3bb5addbd
alexandre <alexandre020602@gmail.com>
2026-03-20 14:22:46 -0300

    expo-linear-gradient and SafeAreaView /  StatusBar component of react native

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,35 +1,25 @@
-import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { Text, View, TouchableOpacity, StatusBar } from "react-native";
 import useTheme from "@/hooks/useTheme";
-import { ColorScheme } from "@/context/themeContext";
+import { createHomeStyles } from "@/assets/style/home.style";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { LinearGradient } from "expo-linear-gradient"
 
 export default function Index() {
 
   const { toogleDarkMode, colors } = useTheme()
-  const styles = createStyle(colors)
+  const homestyles = createHomeStyles(colors)
 
   return (
-    <View style={styles.container}>
-      <Text>Edit theme</Text>
-      <TouchableOpacity onPress={toogleDarkMode}>
-        <Text>Toggle dark mode</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient style={homestyles.container} colors={colors.gradients.background}>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homestyles.container}>
+        <Text>Edit theme</Text>
+        <TouchableOpacity onPress={toogleDarkMode}>
+          <Text>Toggle dark mode</Text>
+        </TouchableOpacity>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
 
 
-const createStyle = (colors: ColorScheme) => {
-  const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: colors.bg,
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    content: {
-      fontSize: 12
-    }
-  })
-
-  return styles
-}

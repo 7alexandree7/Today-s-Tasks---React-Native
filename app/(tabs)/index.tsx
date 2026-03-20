07369309cc0288f0361e63f125cf48cf71efded1
alexandre <alexandre020602@gmail.com>
2026-03-20 03:36:53 -0300

    test query and mutation convex

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,33 @@
-import { Text, View } from "react-native";
+import { Text, View, StyleSheet, TouchableOpacity } from "react-native";
+import { api } from "@/convex/_generated/api";
+import useTheme from "@/hooks/useTheme";
+import { useMutation, useQuery } from "convex/react";
 
 export default function Index() {
+
+  const { toogleDarkMode } = useTheme()
+
+  const todos = useQuery(api.todos.getTodos)
+  const addTodo = useMutation(api.todos.addTodo)
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+    <View style={styles.container}>
+      <Text>Edit theme</Text>
+      <TouchableOpacity onPress={toogleDarkMode}>
+        <Text>Toggle dark mode</Text>
+      </TouchableOpacity>
+      <TouchableOpacity onPress={() => addTodo({ text: "Walk the dog" })}>
+        <Text>Add todo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  }
+});

985fbb2209d1be83722e3af6dce37739df6c8dfd
alexandre <alexandre020602@gmail.com>
2026-03-22 21:23:35 -0300

    refactor: replace dark mode toggle with todo list rendering

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,15 +1,25 @@
-import { Text, View, TouchableOpacity, StatusBar } from "react-native";
+import { Text, View, TouchableOpacity, StatusBar, FlatList } from "react-native";
 import useTheme from "@/hooks/useTheme";
 import { createHomeStyles } from "@/assets/style/home.style";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { LinearGradient } from "expo-linear-gradient"
 import Header from "@/Components/Header";
 import TodoInput from "@/Components/TodoInput";
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
+import LoadingSpinner from "@/Components/LoadingSpinner";
+import RenderTodoItem from "@/Components/RenderTodoItem";
+import EmptyState from "@/Components/EmptyState";
+
 
 export default function Index() {
 
-  const { toogleDarkMode, colors } = useTheme()
+  const { colors } = useTheme()
   const homestyles = createHomeStyles(colors)
+  const todos = useQuery(api.todos.getTodos)
+  const isLoading = todos === undefined
+
+  if (isLoading) return <LoadingSpinner />
 
   return (
     <LinearGradient style={homestyles.container} colors={colors.gradients.background}>
@@ -17,9 +27,16 @@ export default function Index() {
       <SafeAreaView style={homestyles.container}>
         <Header />
         <TodoInput />
-        <TouchableOpacity onPress={toogleDarkMode}>
-          <Text>Toggle dark mode</Text>
-        </TouchableOpacity>
+
+        <FlatList
+          data={todos}
+          renderItem={({item}) => <RenderTodoItem item={item} />}
+          keyExtractor={(item) => item._id}
+          style={homestyles.todoList}
+          contentContainerStyle={homestyles.todoListContent}
+          ListEmptyComponent={<EmptyState />}
+          showsVerticalScrollIndicator={false}
+        />
       </SafeAreaView>
     </LinearGradient>
   );

655e29881f5dbbbdf47c97fe231a66d29bf0e166
alexandre <alexandre020602@gmail.com>
2026-03-23 03:17:50 -0300

    refactor settings component to use LinearGradient and SafeAreaView

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,10 +1,29 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { ScrollView } from 'react-native'
+import { LinearGradient } from 'expo-linear-gradient'
+import useTheme from '@/hooks/useTheme'
+import { createSettingsStyles } from '@/assets/style/settings.style'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import HeaderSettings from '@/Components/HeaderSettings'
+import ProgressStats from '@/Components/ProgressStats'
+import Preferences from '@/Components/Preferences'
+import DangerZone from '@/Components/DangerZone'
 
 export default function settings() {
+
+  const { colors } = useTheme()
+  const settingsStyles = createSettingsStyles(colors)
+
   return (
-    <View>
-      <Text>settings</Text>
-    </View>
+    <LinearGradient style={settingsStyles.container} colors={colors.gradients.background}>
+      <SafeAreaView style={settingsStyles.safeArea}>
+        <HeaderSettings />
+
+        <ScrollView showsVerticalScrollIndicator={false} contentContainerStyle={settingsStyles.content} style={settingsStyles.scrollView}>
+          <ProgressStats />
+          <Preferences />
+          <DangerZone />
+        </ScrollView>
+      </SafeAreaView>
+    </LinearGradient>
   )
 }
